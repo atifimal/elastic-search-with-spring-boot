c3f140bf2ead8e9597e3b09d5946201e4e3cd5f5
Atif Imal <mehmetatif@mail.ru>
2022-05-30 00:04:08 +0300

    Empty spring boot project

---
+++ src/main/java/com/atifimal/data/elasticsearch/basic/elasticsearchwithspringdata/ElasticSearchWithSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.atifimal.data.elasticsearch.basic.elasticsearchwithspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchWithSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchWithSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atifimal/data/elasticsearch/basic/elasticsearchwithspringdata/ElasticSearchWithSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atifimal.data.elasticsearch.basic.elasticsearchwithspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchWithSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
